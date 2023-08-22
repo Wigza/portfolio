a39c47d15c629e475ba4b27ad9ee91a47ec44913
Tim Phillips <timphillips1991@gmail.com>
2023-08-22 17:28:28 +1000

    initial update
    creating sveltekit app
    adding tailwind
    creating large screen site nav

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,33 @@
+<script>
+    import "../app.css";
+    import "../global.css";
+</script>
+
+<style>
+  @import url('https://fonts.googleapis.com/css2?family=Work+Sans:ital,wght@0,300;0,400;0,500;1,300;1,400;1,500&display=swap');
+</style>
+
+<nav class="w-full flex items-center justify-between md:mt-6 md:px-6 fixed top-0">
+    <div class="flex justify-between md:justify-normal rounded-xl bg-white/75 backdrop-blur-lg border-slate-200 border p-1 shadow shadow-slate-100">
+        <a class="font-medium text-slate-700 px-3 py-0.5 rounded-lg hover:bg-slate-50 hover:text-slate-900" 
+            href="/">
+            Tim Phillips, Designer
+        </a>
+        <a class="text-slate-700 px-3 py-0.5 rounded-lg hover:bg-slate-50 hover:text-slate-900" 
+            href="/about">
+            About
+        </a>
+    </div>
+    <div class="hidden md:flex p-1">
+        <a class="text-slate-600 px-3 py-0.5 rounded-lg hover:bg-slate-50 hover:text-slate-900" href="#">Resume</a>
+        <a class="text-slate-600 px-3 py-0.5 rounded-lg hover:bg-slate-50 hover:text-slate-900 flex items-center" href="www.linkedin.com">
+            LinkedIn
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="ml-1 w-5 h-5">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 19.5l15-15m0 0H8.25m11.25 0v11.25" />
+            </svg>              
+        </a>
+    </div>
+</nav>
+
+
+<slot />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,6 @@
-<h1>Welcome to SvelteKit</h1>
-<button>hello
-</button>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="container mt-32 mx-auto px-8 md:max-w-3xl">
+    <div class="text-lg ">
+        <p class="text-slate-700">Tim Phillips is a designer & front-end developer from Melbourne. He’s currently working as a product designer at carsales, Australia’s largest online automotive marketplace.</p>
+        <p class="text-slate-700 pt-4">He is passionate about the craft involved in making intuitive, cohesive and consistent experiences. He also advocates for the role design plays to solve complex problems. </p>
+    </div>
+</div>
