05137dac3162881e500872104afedfaad6ff757f
Tim Phillips <timphillips1991@gmail.com>
2023-08-23 16:17:21 +1000

    dark layout switcher
    adding the ability to toggle between dark/light themes

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,13 @@
 <script>
     import "../app.css";
     import "../global.css";
+
+    import ThemeToggle from "../lib/ThemeToggle.svelte";
+
+    function toggle() {
+        let htmlBody = document.querySelector("html");
+        htmlBody.classList.toggle("dark");
+	}
 </script>
 
 <style>
@@ -9,24 +16,25 @@
 
 <nav class="w-full flex items-center justify-between fixed top-0 md:mt-6 md:px-6">
     <!-- sml screen menu -->
-    <div class="w-full px-2 pt-4 pb-2 flex justify-between bg-white/75 backdrop-blur-lg border-slate-200 border-b shadow shadow-slate-100 md:w-fit md:justify-normal md:rounded-xl md:border">
-        <a class="font-medium text-slate-700 px-3 py-0.5 rounded-lg hover:bg-slate-50 hover:text-slate-900" 
+    <div class="w-full px-2 pt-4 pb-2 flex justify-between bg-white/75 dark:bg-slate-900/75 backdrop-blur-lg border-slate-200 dark:border-slate-700 border-b shadow shadow-slate-100 dark:shadow-slate-900 md:pt-2 md:w-fit md:justify-normal md:rounded-xl md:border">
+        <a class="font-medium text-slate-700 px-3 py-0.5 rounded-lg hover:bg-slate-50 dark:text-slate-300 dark:hover:bg-white/[.05] hover:text-slate-900 dark:hover:text-slate-50" 
             href="/">
             Tim Phillips, Designer
         </a>
-        <a class="text-slate-700 px-3 py-0.5 rounded-lg hover:bg-slate-50 hover:text-slate-900" 
+        <a class="text-slate-700 px-3 py-0.5 rounded-lg hover:bg-slate-50 hover:text-slate-900 dark:text-slate-300 dark:hover:bg-white/[.05] dark:hover:text-slate-50" 
             href="/about">
             About
         </a>
     </div>
     <div class="hidden md:flex p-1">
-        <a class="text-slate-600 px-3 py-0.5 rounded-lg hover:bg-slate-50 hover:text-slate-900" href="#">Resume</a>
-        <a class="text-slate-600 px-3 py-0.5 rounded-lg hover:bg-slate-50 hover:text-slate-900 flex items-center" href="www.linkedin.com">
+        <a class="text-slate-600 px-3 py-0.5 rounded-lg hover:bg-slate-50 hover:text-slate-900 dark:text-slate-300 dark:hover:bg-white/[.05] dark:hover:text-slate-50" href="#">Resume</a>
+        <a class="text-slate-600 px-3 py-0.5 rounded-lg hover:bg-slate-50 hover:text-slate-900 dark:text-slate-300 dark:hover:bg-white/[.05] dark:hover:text-slate-50 flex items-center" href="https://www.linkedin.com/in/tim-phillips-975239b9/" target="_blank">
             LinkedIn
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="ml-1 w-5 h-5">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 19.5l15-15m0 0H8.25m11.25 0v11.25" />
-            </svg>              
         </a>
+        <ThemeToggle 
+            on:click={toggle} 
+            classList="text-slate-600 px-3 py-0.5 rounded-lg hover:bg-slate-50 hover:text-slate-900 dark:text-slate-300 dark:hover:bg-white/[.05] dark:hover:text-slate-50 flex items-center"
+        />
     </div>
 </nav>
 
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,16 @@
+<script>
+    import imageCsHero from '$lib/assets/cs_hero_large.png'
+</script>
+
 <div class="container mt-32 mx-auto px-8 md:max-w-3xl">
-    <div class="text-lg ">
-        <p class="text-slate-700">Tim Phillips is a designer & front-end developer from Melbourne. He’s currently working as a product designer at carsales, Australia’s largest online automotive marketplace.</p>
-        <p class="text-slate-700 pt-4">He is passionate about the craft involved in making intuitive, cohesive and consistent experiences. He also advocates for the role design plays to solve complex problems. </p>
+    <div class="text-2xl text-slate-700 leading-relaxed md:text-lg dark:text-slate-200">
+        <p class="">Tim Phillips is a designer & front-end developer from Melbourne. He’s currently working as a product designer at carsales, Australia’s largest online automotive marketplace.</p>
+        <p class="hidden pt-4 md:block">He is passionate about the craft involved in making intuitive, cohesive and consistent experiences. He also advocates for the role <span class="font-medium">design</span> plays to <span class="font-medium">solve</span> complex <span class="font-medium">problems</span>. </p>
+    </div>
+</div>
+
+<div class="container mx-auto px-8 md:max-w-7xl">
+    <div class="bg-slate-50 border border-slate-100">
+        <img src={imageCsHero} alt="carsales">
     </div>
 </div>
